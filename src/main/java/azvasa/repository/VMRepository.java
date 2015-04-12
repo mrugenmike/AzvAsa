cff749526645bce162e6d6e151f60f4a267be954
Mrugen Deshmukh <mrugenmike@gmail.com>
2015-04-11 18:05:55 -0700

    marked vm repos as an autowireable bean

--- src/main/java/azvasa/repository/VMRepository.java
+++ src/main/java/azvasa/repository/VMRepository.java
@@ -6,12 +6,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Component;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 import java.util.logging.Logger;
 
+@Component
 public class VMRepository {
     org.slf4j.Logger logger = LoggerFactory.getLogger("VMRepository");
     @Autowired
