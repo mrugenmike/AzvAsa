fe90b91d5016c2e341a76c5675f5e8d4d2bcf50a
Mrugen Deshmukh <mrugenmike@gmail.com>
2015-05-03 18:50:42 -0700

    added implementation for the alarms to deliver email

--- app/src/main/java/azvasa/controller/AlarmMonitor.java
+++ app/src/main/java/azvasa/controller/AlarmMonitor.java
@@ -1,27 +1,17 @@
 package azvasa.controller;
 
 import java.lang.Integer;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
+import java.text.SimpleDateFormat;
+import java.util.*;
 import java.util.Map;
 
-import azvasa.repository.SignUpException;
 import azvasa.services.EmailService;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import com.vmware.vim25.VirtualMachineQuickStats;
 import com.vmware.vim25.mo.Folder;
 import com.vmware.vim25.mo.InventoryNavigator;
-import com.vmware.vim25.mo.ManagedEntity;
 import com.vmware.vim25.mo.ServiceInstance;
-import com.vmware.vim25.mo.VirtualMachine;
 
-import org.springframework.boot.autoconfigure.elasticsearch.ElasticsearchAutoConfiguration;
-import org.springframework.dao.DuplicateKeyException;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.dao.DataAccessException;
-import org.springframework.dao.DuplicateKeyException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -33,61 +23,50 @@ public class AlarmMonitor{
 	@Autowired
 	JdbcTemplate template;
 
-	 @Autowired
-	 ServiceInstance serviceInstance;
+	@Autowired
+	EmailService emailService;
 
 	static Long lastExecutionTime ;
-	//private static String lastSearchTimeStamp = "2015-04-26T02:58:00Z";
-
-
-	@Scheduled(fixedRate = 1000000)
+	@Scheduled(fixedDelay = 10000)
 	public void monitorUserAlarm() throws Exception
 	{
 		lastExecutionTime = new Date().getTime();
-
-		EmailService emailService = new EmailService();
-		InventoryNavigator navigator = new InventoryNavigator(serviceInstance.getRootFolder());
 		String alarms = "SELECT username, vm_name, alarmName, description, alarmMetric , alarmOperator , alarmThresholdValue , email , status " +
 				"FROM azvasa.alarm WHERE status = 'Running'" ;
 
 		List<Map<String, Object>> rows = template.queryForList(alarms);
 		if ((rows != null) || (rows.size() > 0)) {
-			Folder rootFolder = serviceInstance.getRootFolder();
 			for (Map<String, Object> tempRow : rows)
 			{
 				String vmName = tempRow.get("vm_name").toString();
 				String alarmName = tempRow.get("alarmName").toString();
-				String alarmThresholdValue = tempRow.get("alarmThresholdValue").toString();
+				int alarmThresholdValue = Integer.parseInt(tempRow.get("alarmThresholdValue").toString());
 				String alarmMetric = tempRow.get("alarmMetric").toString();
 				String email = tempRow.get("email").toString();
 				String alarmOperator = tempRow.get("alarmOperator").toString();
 
-				//from data from VM
-				/*
-				VirtualMachine vm = (VirtualMachine) navigator.searchManagedEntity("VirtualMachine", vmName);
-				VirtualMachineQuickStats stas = vm.getSummary().getQuickStats();
+				ElasticSearchService elasticSearchService = new ElasticSearchService();
+				SimpleDateFormat sdf = new SimpleDateFormat("YYYY-mm-dd'T'HH:mm:ss.SSSZ");
+				final String executionTime = sdf.format(new Date(lastExecutionTime));
 
-				if (alarmMetric.equals("cpu"))
-				{
-					if (stas.overallCpuUsage > Integer.parseInt(alarmThresholdValue)) {
-						//pop up
-						//update alarm status in db
-						emailService.sendEmail(email, " The memory usage for VM " + vm.getName() + " has exceeded the threshold limit.", "Your CPU usage for VM " + vm.getName() + " is " + stas.overallCpuUsage + ". It has exceeded the threshold value");
+				final int threshold = alarmThresholdValue * 100;
+				if(alarmOperator.equalsIgnoreCase("greaterThan")){
+					final List<Map<String, Object>> results = ElasticSearchService.searchLogsGreaterThan(vmName, executionTime,alarmMetric, threshold,10);
+					for(Map<String,Object>result:results){
+						if(Integer.parseInt(result.get(alarmMetric).toString())>=threshold){
+							emailService.sendEmail(email,"Alert from Azvasa",String.format("<b>Alarm with name %s for your VM %s has been triggered</b>",alarmName,vmName));
+							break;
+						}
+					}
+				}else{
+					final List<Map<String, Object>> results = ElasticSearchService.searchLogsLesserThan(vmName, executionTime, alarmMetric, alarmThresholdValue * 100, 10);
+					for(Map<String,Object>result:results){
+						if(Integer.parseInt(result.get(alarmMetric).toString())<=threshold){
+							emailService.sendEmail(email,"Alert from Azvasa",String.format("<b>Alarm with name %s for your VM %s has been triggered</b>",alarmName,vmName));
+							break;
+						}
 					}
 				}
-				else
-				{
-					if(stas.guestMemoryUsage > Integer.parseInt(alarmThresholdValue))
-					{
-						//pop up
-						emailService.sendEmail(email, " The memory usage for VM" + vm.getName() + " has exceeded the threshold limit.", "Your memory usage for VM " + vm.getName() + " is " + stas.guestMemoryUsage + ". It has exceeded the threshold limit");
-					}
-				}*/
-
-				// get data from elastic search
-				ElasticSearchService elasticSearchService = new ElasticSearchService();
-				ElasticSearchService.searchLogs(vmName , lastExecutionTime, alarmMetric ,alarmThresholdValue );
-
 			}
 		}
 	}
